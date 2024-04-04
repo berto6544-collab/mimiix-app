cbdd48f8c45e70300e63865df0ed401b432f38ef
Roberto <root@Robertos-MacBook-Pro.local>
2024-04-04 23:02:02 +1100

    first commit

--- API/API.js
+++ API/API.js
@@ -1,3 +1,4 @@
+//UserData when logged grabs users Data API
 export async function userData(){
     
    const data = fetch('https://mymiix.com/public/api/userData')
@@ -8,7 +9,7 @@ export async function userData(){
 }
 
 
-//login
+//Grabbing Feed items data API 
 export async function FeedsData(start){
     
     const data = fetch('https://mymiix.com/public/api/postsNative?start='+start)
